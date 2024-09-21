7ac97e4366893a2fde9c2bfb3dddc8bdb7bbe6da
sakschmidt <sarah.schmidt@code.seat>
2024-09-21 20:31:05 +0200

    init repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# Manual deployment
+
+Desplegar en Github Pages de forma manual:
+
+- Tenemos un repo en Github.
+- Queremos desplegar una app.
+- Realizar el despliegue manual.
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Manual deployment</h1>
+      <p>Desplegar en Github Pages de forma manual:</p>
+      <ul style={{ listStyle: "none" }}>
+        <li>Tenemos un repo en Github.</li>
+        <li>Queremos desplegar una app.</li>
+        <li>Realizar el despliegue manual.</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
